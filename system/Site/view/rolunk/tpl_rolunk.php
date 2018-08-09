0e96852118c9b48fe9bf3db47c93e159f23d972b
varnagy-attila <vucuka@gmail.com>
2018-08-09 22:22:47 +0200

    Rólunk

--- system/Site/view/rolunk/tpl_rolunk.php
+++ system/Site/view/rolunk/tpl_rolunk.php
@@ -1,20 +1,15 @@
-<?php use System\Libs\Language as Lang; ?>
+<?php
+
+use System\Libs\Language as Lang; ?>
+
+<div class="home-banner style-4">
+</div>
+
 <div id="content" class="container-fluid">
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-                <div class="breadcrumbs">
-                    <span class="clickable"><a href="<?php echo $this->request->get_uri('site_url');?>"><?php echo Lang::get('menu_home'); ?></a></span>
-                    <span class="delimiter">/</span>
-                    <span class="active-page"><?php echo Lang::get('menu_rolunk'); ?></span>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <?php echo $body; ?>
+<?php echo $body; ?>
             </div>
         </div>
     </div>
