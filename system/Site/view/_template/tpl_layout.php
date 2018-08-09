268ab47f6f550801f95349eb4871013505af33ff
varnagy-attila <vucuka@gmail.com>
2018-08-09 21:08:23 +0200

    Követő kódok eltávolítása

--- system/Site/view/_template/tpl_layout.php
+++ system/Site/view/_template/tpl_layout.php
@@ -26,12 +26,7 @@ use System\Libs\Auth;
 		
 	        <?php if (ENV == "production") { ?>
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TM4ZP');</script>
-<!-- End Google Tag Manager -->
+
 
         <?php } ?> 	
 		
@@ -68,21 +63,11 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     <body>
 	<?php if (ENV == "production") { ?>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TM4ZP"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
 <!-- End Google Tag Manager (noscript) -->
 	<?php } ?>
 
-        <div id="fb-root"></div>
-        <script>(function (d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id))
-                    return;
-                js = d.createElement(s);
-                js.id = id;
-                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));</script>
+
 
         <?php include($this->path('tpl_head')); ?>
         <?php include($this->path('content')); ?>
