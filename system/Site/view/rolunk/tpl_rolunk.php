2dbce45a116742eea3721855e46c52afec459102
varnagy-attila <vucuka@gmail.com>
2018-08-10 14:08:42 +0200

    Eltávolítottam a korábban hozzáadott elemet.

--- system/Site/view/rolunk/tpl_rolunk.php
+++ system/Site/view/rolunk/tpl_rolunk.php
@@ -2,9 +2,6 @@
 
 use System\Libs\Language as Lang; ?>
 
-<div class="home-banner style-4">
-</div>
-
 <div id="content" class="container-fluid">
     <div class="container">
         <div class="row">
