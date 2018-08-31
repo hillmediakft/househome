5b22931ad060e25f4992016b57dace36b676ce4f
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2018-08-31 13:53:24 +0200

    getBackground() metodus hozzaadva. - kezdokep nevenek lekerdezese a slider tablabol

--- system/Site/Model/Home_model.php
+++ system/Site/Model/Home_model.php
@@ -10,5 +10,21 @@ class Home_model extends SiteModel {
 	}
 
 
+    /**
+     * Kezdőoldal háttérkép visszaadása
+     * (az összes kép nevét visszaadja sorrendben)
+     * 	
+     * @return Array (az összes slide minden adata a "slider_order" szerint rendezve)
+     */
+    public function getBackground()
+    {
+		$this->query->set_table('slider');
+		$this->query->set_columns('picture');
+		$this->query->set_where('active', '=', 1);
+        $this->query->set_orderby(array('slider_order'));
+        $result = $this->query->select();
+        return (!empty($result)) ? $result[0]['picture'] : '';
+    }
+
 }
 ?>
